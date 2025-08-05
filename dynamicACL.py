9d46e794dfa99fc4437b09fc7a10429574661897
Patrick Felt <pfelt@arista.com>
2025-08-05 15:47:08 -0600

    - add a copyright banner
    Signed-off-by: Patrick Felt <pfelt@arista.com>

--- dynamicACL.py
+++ dynamicACL.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+# Copyright (c) 2025 Arista Networks, Inc.
+# Use of this source code is governed by the MIT license
+# that can be found in the LICENSE file.
+"""
+
 import pyeapi, argparse, uuid
 import dns.resolver
 
