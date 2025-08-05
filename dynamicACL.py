9b63bf829e807edafedd4825d921c2e25d3a61a0
Patrick Felt <pfelt@arista.com>
2025-08-05 12:48:30 -0600

    first commit

---
+++ dynamicACL.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import pyeapi, argparse
+import dns.resolver
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-hosts', required=True, nargs='+', help="dns hostnames to resolve.  multiple entries can be specified space separated")
+parser.add_argument('-acl', default="DYNAMICACL", help="name of the acl to replace post resolution")
+parser.add_argument('-recordType', default="a", help="a or aaaa")
+parser.add_argument('-lineFormat', default='permit ip any host {}', help="a python format string to be used for each host line")
+parser.add_argument('-preLines', nargs='+', help="any acl lines to include before dns resolves space delimted and wrapped in quotes")
+parser.add_argument('-postLines', nargs='+', help="any acl lines to include after dns resolves space delimted and wrapped in quotes")
+
+args = parser.parse_args()
+
+hosts = []
+if args.preLines:
+    hosts.append("remark preLines")
+    hosts.extend(args.preLines)
+
+# do the dns resolve
+for host in args.hosts:
+    hosts.append(f"remark {host}")
+
+    r = dns.resolver.Resolver(configure=True)
+    try:
+        answer = r.resolve(host, args.recordType)
+    except dns.resolver.NXDOMAIN:
+        continue
+    except dns.resolver.NoAnswer:
+        continue
+
+    for rr in answer:
+        hosts.append(args.lineFormat.format(rr))
+
+if args.postLines:
+    hosts.append("remark postLines")
+    hosts.extend(args.postLines)
+
+if len(hosts):
+    # we have hosts now, let's go ahead and rewrite the acl
+    family = "ip" if args.recordType == "a" else "ipv6"
+    cmds = [
+        "configure session dynamicacl",
+        f"no {family} access-list {args.acl}",
+        f"{family} access-list {args.acl}"
+    ]
+    cmds.extend(hosts)
+    cmds.append("commit")
+    client = pyeapi.connect_to('localhost')
+
+    result = client.run_commands(cmds, autoComplete=True, enable=True)
---
+++ dynamicACL.spec
@@ -0,0 +1,28 @@
+%define _rpmfilename %%{NAME}-%%{VERSION}.%%{ARCH}.rpm
+
+Name:           dynamicACL
+Version:        1.0.1
+Release:        1%{?dist}
+Summary:        Dynamic configuration of ACLs based on FQDN
+BuildArch:	noarch
+
+License:        GPL
+URL:            https://github.com/arista-rockies/dynamicACL
+Source0:	https://github.com/arista-rockies/dynamicACL/archive/refs/tags/dynamicACL-v%{version}.tar.gz
+
+%description
+Script to create a custom acl using FQDN objects. Schedule this command in eos with something like: `schedule tester interval 2 timeout 1 max-log-files 0 command bash sudo ip netns exec ns-MGMT /mnt/flash/dynamicACL.py -hosts www.google.com www.novell.com www.arista.com -recordType aaaa`
+
+%prep
+%setup
+
+%install
+mkdir -p %{buildroot}/mnt/flash
+install -m 755 dynamicACL.py %{buildroot}/mnt/flash/dynamicACL.py
+
+%files
+/mnt/flash/dynamicACL.py
+
+%changelog
+* Tue Aug 05 2025 Patrick Felt <pfelt@arista.com>
+- 
